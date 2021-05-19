6906672e0ef180a5eb003639d6dc4b4d7af6c24b
wangchengbing329 <319264106@qq.com>
2021-05-19 23:58:48 +0800

    try

--- README.md
+++ README.md
@@ -4,9 +4,10 @@ ___________________
 > 初始化命令 npm init egg <projectName>             
 
 ### 项目目录理解
-      __route__  接口路径
-      **controller** 暴露出去给路由使用的方法
-      **service**    通常业务逻辑都在这个文件
-      **model**     通常CRUD或拼接sql语句都在这个文件夹中
-### 准备只是
+  __route__  接口路径
+  **controller** 暴露出去给路由使用的方法
+  **service**    通常业务逻辑都在这个文件
+  **model**     通常CRUD或拼接sql语句都在这个文件夹中
+### 准备知识
   sequlize + mysql
+  <u>注意sequelize配置<u>
--- egg-mvc/config/config.default.ts
+++ egg-mvc/config/config.default.ts
@@ -3,6 +3,12 @@ import { EggAppConfig, EggAppInfo, PowerPartial } from 'egg';
 export default (appInfo: EggAppInfo) => {
   const config = {} as PowerPartial<EggAppConfig>;
 
+  config.sequelize = {
+    dialect: "mysql",
+    host: "127.0.0.1",
+    port: "3306",
+    database: "egg-sequelizenpm install --save-dev sequelize-cli-doc-default"
+  }
   // override config from framework / plugin
   // use for cookie sign key, should change to your own and keep security
   config.keys = appInfo.name + '_1621350258666_5723';
